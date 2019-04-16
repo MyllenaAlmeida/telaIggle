66dbbe9e5ade22f540b47c79cccb1f9a8fd78ae6
MyllenaAlmeida <38386226+MyllenaAlmeida@users.noreply.github.com>
2019-04-15 23:51:16 -0300

    atualizando navegação

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
-import { createStackNavigator } from "react-navigation"
+import { createAppContainer, createStackNavigator } from "react-navigation";
 import Signup from "./pages/signup";
 
-export default createStackNavigator ({
-    Signup,
-}, {
-    navigationOptions: {
+const AppNavigator = createStackNavigator(
+  {Signup}, {
+      navigationOptions: {
       headerStyle: { backgroundColor: "#3CB371" },
       headerTintColor: "#FFF"
     },
 });
+
+const AppContainer = createAppContainer(AppNavigator);
+export default AppContainer;
